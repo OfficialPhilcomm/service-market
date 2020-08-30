66e65912acb5e7a71db93cc53da2c38d77cc5d9d
Philipp Schlesinger <info@philcomm.dev>
2020-08-30 16:49:25 +0200

    Fill create popup

--- src/api/PokeAPI.js
+++ src/api/PokeAPI.js
@@ -3,6 +3,8 @@ import axios from "axios";
 export default class PokeAPI {
   static pokemonList = [];
 
+  static itemList = [];
+
   static POKE_API_BASE_URL = "https://pokeapi.co/api/v2/";
 
   static async requestPokemonList() {
@@ -11,6 +13,30 @@ export default class PokeAPI {
       url: this.POKE_API_BASE_URL + "pokemon?limit=647",
     });
 
-    this.pokemonList = response.data;
+    this.pokemonList = response.data.results;
+
+    const itemResponse = await axios({
+      method: "get",
+      url: this.POKE_API_BASE_URL + "item?limit=954",
+    });
+
+    this.itemList = itemResponse.data.results;
+  }
+
+  static async getAdvancedData(index) {
+    const p = this.pokemonList[index];
+    if (p.data) {
+      return p;
+    } else {
+      const response = await axios({
+        method: "get",
+        url: p.url,
+      });
+      this.pokemonList[index] = {
+        ...this.pokemonList[index],
+        ...{ data: response.data },
+      };
+      return this.pokemonList[index];
+    }
   }
 }
--- src/components/popups/create_order_popup/CreateOrderPopup.js
+++ src/components/popups/create_order_popup/CreateOrderPopup.js
@@ -1,11 +1,181 @@
 import React, { Component } from "react";
 import CloseablePopup from "../CloseablePopup";
+import { ApplicationContext } from "../../../contexts/ApplicationContext";
+import PokeAPI from "../../../api/PokeAPI";
+import StringUtils from "../../../api/StringUtils";
+import "./CreateOrderPopup.css";
 
 export default class CreateOrderPopup extends Component {
+  static contextType = ApplicationContext;
+
+  state = {
+    p: null,
+  };
+
+  async componentDidMount() {
+    const pokemon = await PokeAPI.getAdvancedData(0);
+    this.setState({ p: pokemon });
+  }
+
+  switchPokemon = (event) => {
+    const index = parseInt(event.target.value);
+    this.displayPokemonData(index);
+  };
+
+  displayPokemonData = async (index) => {
+    const pokemon = await PokeAPI.getAdvancedData(index);
+    this.setState({ p: pokemon });
+  };
+
   render() {
+    const p = this.state.p;
+    console.log(p);
+
     return (
       <CloseablePopup closeCallback={this.props.closeCallback}>
-        This is a closeable popup
+        <div className="content">
+          <div className="create-row">
+            <select onChange={this.switchPokemon}>
+              {PokeAPI.pokemonList.map((pokemon, index) => (
+                <option value={index} key={index}>
+                  {StringUtils.humanize(pokemon.name)}
+                </option>
+              ))}
+            </select>
+            <span>lvl</span>
+            <select>
+              <option value="50">50</option>
+              <option value="100">100</option>
+            </select>
+          </div>
+          {p ? (
+            <React.Fragment>
+              <div className="create-row">
+                <select>
+                  {p.data.abilities.map((ability) => (
+                    <option
+                      value={ability.ability.name}
+                      key={ability.ability.name}
+                    >
+                      {StringUtils.humanize(ability.ability.name)}
+                    </option>
+                  ))}
+                </select>
+                <select>
+                  {PokeAPI.itemList.map((item) => (
+                    <option value={item.name} key={item.name}>
+                      {StringUtils.humanize(item.name)}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="create-row">
+                <table>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <select>
+                          {p.data.moves.map((move) => (
+                            <option value={move.move.name} key={move.move.name}>
+                              {StringUtils.humanize(move.move.name)}
+                            </option>
+                          ))}
+                        </select>
+                      </td>
+                      <td>
+                        <select>
+                          {p.data.moves.map((move) => (
+                            <option value={move.move.name} key={move.move.name}>
+                              {StringUtils.humanize(move.move.name)}
+                            </option>
+                          ))}
+                        </select>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <select>
+                          {p.data.moves.map((move) => (
+                            <option value={move.move.name} key={move.move.name}>
+                              {StringUtils.humanize(move.move.name)}
+                            </option>
+                          ))}
+                        </select>
+                      </td>
+                      <td>
+                        <select>
+                          {p.data.moves.map((move) => (
+                            <option value={move.move.name} key={move.move.name}>
+                              {StringUtils.humanize(move.move.name)}
+                            </option>
+                          ))}
+                        </select>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="create-row">
+                <table>
+                  <tbody>
+                    <tr>
+                      <th />
+                      <th>HP</th>
+                      <th>ATK</th>
+                      <th>DEF</th>
+                      <th>SPATK</th>
+                      <th>SPDEF</th>
+                      <th>SPEED</th>
+                    </tr>
+                    <tr>
+                      <td>IV</td>
+                      <td>
+                        <input type="number" min="0" max="31" />
+                      </td>
+                      <td>
+                        <input type="number" min="0" max="31" />
+                      </td>
+                      <td>
+                        <input type="number" min="0" max="31" />
+                      </td>
+                      <td>
+                        <input type="number" min="0" max="31" />
+                      </td>
+                      <td>
+                        <input type="number" min="0" max="31" />
+                      </td>
+                      <td>
+                        <input type="number" min="0" max="31" />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>EV</td>
+                      <td>
+                        <input type="number" min="0" max="252" />
+                      </td>
+                      <td>
+                        <input type="number" min="0" max="252" />
+                      </td>
+                      <td>
+                        <input type="number" min="0" max="252" />
+                      </td>
+                      <td>
+                        <input type="number" min="0" max="252" />
+                      </td>
+                      <td>
+                        <input type="number" min="0" max="252" />
+                      </td>
+                      <td>
+                        <input type="number" min="0" max="252" />
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </React.Fragment>
+          ) : null}
+        </div>
+        <div className="buttons"></div>
       </CloseablePopup>
     );
   }
