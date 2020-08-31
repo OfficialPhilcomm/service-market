d9b6e08e3756e8a373bcfcf5503781a485e41c81
Philipp Schlesinger <info@philcomm.dev>
2020-08-31 14:52:45 +0200

    Add logs

--- src/components/layout/navbar/NavBar.js
+++ src/components/layout/navbar/NavBar.js
@@ -4,11 +4,13 @@ import "./NavBar.css";
 
 export default class NavBar extends Component {
   render() {
+    console.log(process.env);
+
     return (
       <nav>
         <div className="title">
           PokeMMO Breeding Market{" "}
-          <span>(alpha {process.env.REACT_APP_VERSION})</span>
+          <span>(alpha {process.env.npm_package_version})</span>
         </div>
       </nav>
     );
