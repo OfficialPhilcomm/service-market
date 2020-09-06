558e36286416d20835f25b3d4ea611e6f84deae2
Philipp Schlesinger <info@philcomm.dev>
2020-09-06 15:36:01 +0200

    Change alpha state to beta state

--- src/components/layout/navbar/NavBar.js
+++ src/components/layout/navbar/NavBar.js
@@ -7,7 +7,7 @@ export default class NavBar extends Component {
     return (
       <nav>
         <div className="title">
-          PokeMMO Breeding Market <span>(alpha)</span>
+          PokeMMO Breeding Market <span>(beta)</span>
         </div>
       </nav>
     );
